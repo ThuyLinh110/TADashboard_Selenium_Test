af7d6b5b45bf513abe3faf4bf610e9896a574883
Hang1809 <hangdinhphankhanh@gmail.com>
2022-12-02 08:45:48 +0700

    update 2.12.22

--- src/test/java/testcases/dashBoardTA/MainPageTest.java
+++ src/test/java/testcases/dashBoardTA/MainPageTest.java
@@ -37,11 +37,6 @@ public class MainPageTest extends BaseTest{
     }
 
     @Test
-    public void TC16(){
-
-    }
-
-    @Test
     public void TC21() throws InterruptedException {
         Log.info("Verify that user is able to edit the name of the page (Parent/Sibbling) successfully");
         Log.info("Step1: Navigate to Dashboard login page");
@@ -72,15 +67,6 @@ public class MainPageTest extends BaseTest{
         WebElementManager.sleep();
         addPage.clickBtnInForm("OK");
         Log.info("User is able to edit the name of parent page successfully");
-
-        //Log.info("Step10: Step Click Ok button on Edit Page dialog");
-        //Log.info("Step11: VP Observe the current page User is able to edit the name of") ;
-        //parent page successfully
-        //Log.info("Step12: Step Go to the second created page Page 2");
-        //Log.info("Step13: Step Click Edit link");
-        //Log.info("Step14: Step Enter another name into Page Name field Page name: Page 4");
-        //Log.info("Step15: Click Ok button on Edit Page dialog");
-        //Log.info("Step16: VP Observe the current page");
 
     }
 
